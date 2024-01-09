c43b14a736209a4002e7a4c6f082b1492da5bef5
mozzam123 <mozzam607@gmail.com>
2024-01-09 15:43:54 +0530

    Methods to empty array

--- emptyArray.js
+++ emptyArray.js
@@ -1,8 +1,17 @@
 // How to empty an array list
 
-
+// Method 1
 var arrayList =  ['a', 'b', 'c', 'd', 'e', 'f'];
 
 arrayList = []
 
 console.log(arrayList);
+
+
+// Method 2
+
+var newArray = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+newArray.length = 0
+
+console.log(newArray);
