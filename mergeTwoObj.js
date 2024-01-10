1365759a22b13deb8e14a8b232da279c7822a8f9
mozzam123 <mozzam607@gmail.com>
2024-01-10 17:45:04 +0530

    how to merge two objects

---
+++ mergeTwoObj.js
@@ -0,0 +1,18 @@
+var person = {
+    name: 'John',
+    age: 24
+}
+
+var address = {
+    addressLine1: 'Some Location x',
+    addressLine2: 'Some Location y',
+    city: 'NewYork'
+}
+
+const mergeObj = (to, from) => {
+    const newobj = Object.assign(to, from)
+    console.log(newobj);
+}
+
+
+console.log(mergeObj(person, address));
