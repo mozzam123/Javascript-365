9bd7201b6a3e03126bfc8ea30f2251b9611becb6
mozzam123 <mozzam607@gmail.com>
2024-01-10 15:57:00 +0530

    Constructor in js

---
+++ constructors.js
@@ -0,0 +1,26 @@
+function Person (name, age, salary){
+    this.name  = name
+    this.age = age
+    this.salary = salary
+    this.incrementSalary = function(byValue){
+        this.salary = this.salary + byValue
+        console.log('Current new salary is: ', this.salary);
+    }
+}
+
+
+function Employee(company){
+    this.company = company
+}
+
+// Prototypal Inheritance
+Employee.prototype = new Person("mozzam", 23, 5000)
+
+var emp1 = new Employee("google")
+
+emp1.incrementSalary(6)
+
+// checking the instance of emp1
+
+console.log(emp1 instanceof Person);
+console.log(emp1 instanceof Employee);
