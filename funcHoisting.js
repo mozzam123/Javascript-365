a15396307fa7ca3b97e1aff39f9cd537e66ad6a9
mozzam123 <mozzam607@gmail.com>
2024-03-04 11:15:46 +0530

    Hoisting in Js

--- funcHoisting.js
+++ funcHoisting.js
@@ -1,11 +1,17 @@
 /*
-So, in summary, due to function hoisting, the code executes without errors and produces the following output
+
+In JavaScript, hoisting is a behavior where variable and function declarations are moved
+to the top of their containing scope during the compilation phase, before the code is executed.
+This means you can use a variable or function before it's declared in your code.
+
 */
 
 bar();
 (function abc() {
   console.log("something");
 })();
+
+
 function bar() {
   console.log("bar got called !!!");
 }
